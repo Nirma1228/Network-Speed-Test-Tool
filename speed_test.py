3c2511757987279eb25e622c435e757ccd8e53a2
Nirma Nethmini <nirmanethmini@gmail.com>
2025-07-08 12:39:34 +0530

    Update speed_test.py

--- speed_test.py
+++ speed_test.py
@@ -2,3 +2,10 @@ import speedtest
 
 def convert_speed(bytes_per_second):
     return round(bytes_per_second / (1024 * 1024), 2)  # Convert to Mbps
+
+# Create Speedtest object
+st = speedtest.Speedtest()
+
+print("Getting best server...")
+st.get_best_server()
+
